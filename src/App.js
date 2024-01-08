b23ab53d69cc2917c8fe0c6e80b80f1e0610cdd5
Micah Wegeleben <micahwegeleben@outlook.com>
2024-01-08 11:44:19 -0800

    add 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <>
         <h1>Hello</h1>
         <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
     </>
   );
 }
