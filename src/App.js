5bae4100a28d76e6115e232dcce548fd2287b68d
Micah Wegeleben <micahwegeleben@outlook.com>
2024-01-15 10:31:49 -0800

    updated readme

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return(
+      <Component3 />
+  );
+}
+
+function Component3(){
   return (
     <>
-        <h1>Hello</h1>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
+      <Component2 />
+      <Component1 />
     </>
   );
 }
+
+function Component1(){
+  return (
+    <>
+      <p>1</p>
+      <p1>2</p1>
+    </>
+  );
+}
+
+function Component2(){
+  return(
+      <h1>This is at the top</h1>
+  );
+}
+
 
 export default App;
